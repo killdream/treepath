158570aa12ec9f3dbeea1e81245290f0506ee037
Quildreen "Sorella" Motta <quildreen@gmail.com>
2013-11-25 18:47:16 -0200

    Only links to the module we use.

--- index.js
+++ index.js
@@ -24,7 +24,7 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-var f     = require('athena')
+var f     = require('athena/lib/higher-order')
 var slice = Function.call.bind([].slice)
 
 map = f.curry(2, map)
